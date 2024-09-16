ddddb71e2466404d6d05be2be5491c60c2e5ef47
Lawrence Cheung <15526007+lawrenceccheung@users.noreply.github.com>
2024-09-15 18:39:15 -0600

    Added HighWS_LowTI template notebooks

---
+++ Runs/HighWS_LowTI.Flight/oneturb_7x2/execnotebooks.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+export TEMPLATE=HighWS_LowTI_oneturb_template.ipynb
+
+export CONFIGSCRIPT=baseline.py
+jupyter nbconvert --to notebook --execute $TEMPLATE --output HighWS_LowTI_oneturb_baseline.ipynb
+
+export CONFIGSCRIPT=WM_Helix_A2_St_0p3.py
+jupyter nbconvert --to notebook --execute $TEMPLATE --output HighWS_LowTI_oneturb_WM_Helix_A2_St_0p3.ipynb
+
+export CONFIGSCRIPT=WM_Helix_A4_St_0p3.py
+jupyter nbconvert --to notebook --execute $TEMPLATE --output HighWS_LowTI_oneturb_WM_Helix_A4_St_0p3.ipynb
+
+export CONFIGSCRIPT=WM_Pulse_A2_St_0p3.py
+jupyter nbconvert --to notebook --execute $TEMPLATE --output HighWS_LowTI_oneturb_WM_Pulse_A2_St_0p3.ipynb
+
+export CONFIGSCRIPT=WM_Pulse_A4_St_0p3.py
+jupyter nbconvert --to notebook --execute $TEMPLATE --output HighWS_LowTI_oneturb_WM_Pulse_A4_St_0p3.ipynb
+
+export CONFIGSCRIPT=WM_SideToSide_A2_St_0p3.py
+jupyter nbconvert --to notebook --execute $TEMPLATE --output HighWS_LowTI_oneturb_WM_SideToSide_A2_St_0p3.ipynb
+
+export CONFIGSCRIPT=WM_SideToSide_A4_St_0p3.py
+jupyter nbconvert --to notebook --execute $TEMPLATE --output HighWS_LowTI_oneturb_WM_SideToSide_A4_St_0p3.ipynb
+
+export CONFIGSCRIPT=WS_m10.py
+jupyter nbconvert --to notebook --execute $TEMPLATE --output HighWS_LowTI_oneturb_WS_m10.ipynb
+
+export CONFIGSCRIPT=WS_m20.py
+jupyter nbconvert --to notebook --execute $TEMPLATE --output HighWS_LowTI_oneturb_WS_m20.ipynb
+
+export CONFIGSCRIPT=WS_p10.py
+jupyter nbconvert --to notebook --execute $TEMPLATE --output HighWS_LowTI_oneturb_WS_p10.ipynb
+
+export CONFIGSCRIPT=WS_p20.py
+jupyter nbconvert --to notebook --execute $TEMPLATE --output HighWS_LowTI_oneturb_WS_p20.ipynb
+
